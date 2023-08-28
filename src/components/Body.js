c1dd4810028091766ce421dd44eff157760ef2e6
sedin-rajith <rajith@sedintechnologies.com>
2023-08-28 10:05:08 +0530

    useEffect hook usage

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ResCard from "./ResCard";
 import { RES_NAMES } from "../utils/constants";
 
@@ -15,6 +15,17 @@ const Body = () => {
         }
         setRated(!rated);
     };
+
+    useEffect(() => {
+        fetchData()
+    },[])
+
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9436747&lng=80.2411929&collection=83649&isNewCollectionFlow=true&tags=layout_CCS_Biryani&sortBy=&filters=&type=rcv2&offset=0&page_type=null")
+        const json = await data.json()
+        console.log("dataaaaa",json.data.cards[3].card)
+        //setFilteredRestaurants(json)
+    }
 
     return (
         <div>
